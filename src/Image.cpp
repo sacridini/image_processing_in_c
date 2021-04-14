fd92c42a6517752e6b4e5007d4e5f7788e121f78
sacridini <eduardolacerdageo@gmail.com>
2021-04-14 19:06:23 -0300

    added convolution function

--- src/Image.cpp
+++ src/Image.cpp
@@ -72,8 +72,31 @@ ImageType Image::getFileType(const char* filename) {
  return PNG;
 }
 
-Image& Image::std_convolve_clamp_to_0(uint8_t channel, uint32_t ker_w, uint32_t ker_h, double[] ker, uint32_t cr, uint32_t cc) {
-  
+// https://www.youtube.com/watch?v=twu-cRlCbG4&ab_channel=CodeBreak
+Image& Image::std_convolve_clamp_to_0(uint8_t channel, uint32_t ker_w, uint32_t ker_h, double ker[], uint32_t cr, uint32_t cc) {
+ uint8_t new_data[w*h];
+ uint64_t center = cr * ker_w + cc;
+ for (uint64_t k = channel; k < size; k += channels) {
+  double c = 0;
+  for (long i = -((long)cr); i < (long)ker_h-cr; ++i) {
+	long row = ((long)k/channels)/w-i;
+	if (row < 0 ||  row > h - 1) {
+	  continue;
+	}
+	for (long j = -((long)cc); j < (long)ker_w-cc; ++j) {
+	  long col = ((long)k/channels)/w-j;
+	  if (col < 0 ||  col > w - 1) {
+		continue;
+	  }
+	  c += ker[center+i*(long)ker_w]*data[(row*w+col)*channels+channel];
+	}
+  }
+  new_data[k/channels] = (uint8_t)BYTE_BOUND(round(c));
+  }
+  for (uint64_t k = channel; k < size; k += channels) {
+	data[k] = new_data[k/channels];
+  }
+  *this;
 }
 
 Image& Image::diffmap(Image& img) {
